f890402a17ab58f02364aa1a077af4ef4c6aa754
Craig McLure <craig@mclure.net>
2022-07-08 14:33:27 +0100

    Load the correct EQ faders depending on device.

--- src/components/sections/mic/Equalizer.vue
+++ src/components/sections/mic/Equalizer.vue
@@ -1,11 +1,11 @@
 <template>
-  <ContentBox title="Equalizer">
+  <ContentBox  title="Equalizer">
     <div class="rowContent" :class="{ hidden: isVisible }">
       <Slider title="Bass" :slider-min-value=-9 :slider-max-value=9 :text-min-value=-9 :text-max-value=9 text-suffix="" :slider-value=0 />
       <Slider title="Mid" :slider-min-value=-9 :slider-max-value=9 :text-min-value=-9 :text-max-value=9 text-suffix="" :slider-value=0 />
       <Slider title="Treble" :slider-min-value=-9 :slider-max-value=9 :text-min-value=-9 :text-max-value=9 text-suffix="" :slider-value=0 />
     </div>
-    <div class="rowContent" :class="{ hidden: !isVisible || deviceType }">
+    <div v-if="this.device_type === 'Mini'" class="rowContent" :class="{ hidden: !isVisible }">
       <Slider title="90Hz" :slider-min-value=-9 :slider-max-value=9 :text-min-value=-9 :text-max-value=9 text-suffix="" :slider-value=0 />
       <Slider title="250Hz" :slider-min-value=-9 :slider-max-value=9 :text-min-value=-9 :text-max-value=9 text-suffix="" :slider-value=0 />
 
@@ -15,7 +15,7 @@
       <Slider title="3Khz" :slider-min-value=-9 :slider-max-value=9 :text-min-value=-9 :text-max-value=9 text-suffix="" :slider-value=0 />
       <Slider title="8KHz" :slider-min-value=-9 :slider-max-value=9 :text-min-value=-9 :text-max-value=9 text-suffix="" :slider-value=0 />
     </div>
-    <div class="rowContent" :class="{ hidden: !isVisible || !deviceType}">
+    <div v-else class="rowContent" :class="{ hidden: !isVisible }">
       <Slider title="31.5Hz" :slider-min-value=-9 :slider-max-value=9 :text-min-value=-9 :text-max-value=9 text-suffix="" :slider-value=0 />
       <Slider title="63Hz" :slider-min-value=-9 :slider-max-value=9 :text-min-value=-9 :text-max-value=9 text-suffix="" :slider-value=0 />
       <Slider title="125Hz" :slider-min-value=-9 :slider-max-value=9 :text-min-value=-9 :text-max-value=9 text-suffix="" :slider-value=0 />
@@ -37,6 +37,8 @@
 import ContentBox from "../../ContentBox";
 import Slider from "../../slider/Slider";
 import ExpandoBox from "../../util/ExpandoBox";
+import {store} from "@/store";
+import {waitFor} from "@/util/util";
 export default {
   name: "MicEqualiser",
   components: {ExpandoBox, Slider, ContentBox},
@@ -44,9 +46,17 @@ export default {
   data() {
     return {
       isVisible: false,
-      //Full = true; Mini = False
-      deviceType: true,
+      device_type: undefined,
     }
+  },
+
+  created() {
+    waitFor(() => store.hasActiveDevice() === true).then(
+        () => {
+          this.device_type = store.getActiveDevice().hardware.device_type;
+          console.log(this.device_type);
+        }
+    );
   },
 
   methods: {
@@ -56,7 +66,7 @@ export default {
 
     toggleExpando() {
       this.isVisible = !this.isVisible;
-    }
+    },
   }
 }
 </script>
