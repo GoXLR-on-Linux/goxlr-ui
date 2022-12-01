b7cd451845827997afe22e2248a1084e80bb0e8a
Craig McLure <craig@mclure.net>
2022-12-01 08:28:12 +0000

    Added License Dialog

--- src/components/sections/System.vue
+++ src/components/sections/System.vue
@@ -8,6 +8,10 @@
       <font-awesome-icon icon="fa-solid fa-circle-question" />
     </BigButton>
     <div style="margin-right: 30px"></div>
+    <BigButton title="License" @button-clicked="showLicenseModal = true">
+      <font-awesome-icon icon="fa-solid fa-circle-question" />
+    </BigButton>
+    <div style="margin-right: 30px"></div>
     <a :href="'https://discord.gg/Wbp3UxkX2j'">
       <BigButton :title="'Help'">
         <font-awesome-icon icon="fa-solid fa-circle-question" />
@@ -52,6 +56,52 @@
     </div>
     <template v-slot:footer>&nbsp;</template>
   </ModalBox>
+
+  <ModalBox width="680px" v-if="showLicenseModal" @close="showLicenseModal = false">
+    <template v-slot:title>Licenses</template>
+    <textarea style="width: 630px; height: 710px; background-color: #2B2F2D; color: #fff; border: 1px solid #000" disabled>Copyright (c) 2022 Nathan Adams
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+-----------------------------------------------------------------------------------
+
+Copyright 2022 Music Tribe Brands CA Ltd.
+
+This code and documentation contain information and intellectual property that
+is owned by MUSIC TRIBE. Permission is hereby granted to any person obtaining a
+copy of this software and associated documentation files to use the information
+without restriction, including without limitation the rights to use, copy, modify,
+merge, publish, distribute, sublicense, and to permit persons to whom the software
+and documentation is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.</textarea>
+    <template v-slot:footer>&nbsp;</template>
+  </ModalBox>
 </template>
 
 
@@ -70,6 +120,7 @@ export default {
     return {
       showMicModal: false,
       showVersionModal: false,
+      showLicenseModal: false,
       setupTitle: "Mic Setup"
     }
   },
