adc3fc465843b8803995765f8e04a686cc1515d4
Craig McLure <craig@mclure.net>
2022-12-11 14:21:21 +0000

    Fixed sliders not mapping to store values

--- src/components/sections/mic/Gate.vue
+++ src/components/sections/mic/Gate.vue
@@ -1,16 +1,23 @@
 <template>
   <ContentBox title="Gate">
     <div class="rowContent" v-show="!isAdvanced()">
-      <Slider title="Amount" :id=0 :slider-min-value=0 :slider-max-value=100 text-suffix="" :slider-value=amount @value-changed="setValue" :store-path="getStorePath('threshold')"/>
+      <Slider title="Amount" :id=0 :slider-min-value=0 :slider-max-value=100 text-suffix="" :slider-value=getAmount()
+              @value-changed="setValue" :store-path="getStorePath('threshold')"/>
     </div>
     <div class="rowContent" v-show="isAdvanced()">
-      <Slider title="Threshold" :id=1 :slider-min-value=-59 :slider-max-value=0 text-suffix="dB" :slider-value=threshold @value-changed="setValue" :store-path="getStorePath('threshold')"/>
-      <Slider title="Attenuation" :id=2 :slider-min-value=0 :slider-max-value=100 text-suffix="%"  :slider-value=attenuation @value-changed="setValue" :store-path="getStorePath('attenuation')"/>
-      <Slider title="Attack" :id=3 :slider-min-value=10 :slider-max-value=2000 text-suffix="ms" :value-map="getGateValueMap()" :slider-value=attack @value-changed="setValue" :store-path="getStorePath('attack')"/>
-      <Slider title="Release" :id=4 :slider-min-value=10 :slider-max-value=2000 text-suffix="ms" :value-map="getGateValueMap()" :slider-value=release @value-changed="setValue" :store-path="getStorePath('release')"/>
+      <Slider title="Threshold" :id=1 :slider-min-value=-59 :slider-max-value=0 text-suffix="dB"
+              :slider-value=getThreshold() @value-changed="setValue" :store-path="getStorePath('threshold')"/>
+      <Slider title="Attenuation" :id=2 :slider-min-value=0 :slider-max-value=100 text-suffix="%"
+              :slider-value=getAttenuation() @value-changed="setValue" :store-path="getStorePath('attenuation')"/>
+      <Slider title="Attack" :id=3 :slider-min-value=10 :slider-max-value=2000 text-suffix="ms"
+              :value-map="getGateValueMap()" :slider-value=getAttack() @value-changed="setValue"
+              :store-path="getStorePath('attack')"/>
+      <Slider title="Release" :id=4 :slider-min-value=10 :slider-max-value=2000 text-suffix="ms"
+              :value-map="getGateValueMap()" :slider-value=getRelease() @value-changed="setValue"
+              :store-path="getStorePath('release')"/>
     </div>
   </ContentBox>
-  <ExpandoBox @expando-clicked="toggleAdvanced" :expanded="isAdvanced()" />
+  <ExpandoBox @expando-clicked="toggleAdvanced" :expanded="isAdvanced()"/>
 </template>
 
 <script>
@@ -23,28 +30,41 @@ import {websocket} from "@/util/sockets";
 export default {
   name: "MicGate",
   components: {Slider, ExpandoBox, ContentBox},
-  data() {
-    return {
-      amount: 15,
-      threshold: 0,
-      attenuation: 10,
-      attack: 10,
-      release: 10
-    }
-  },
-
-  created() {
-    this.waitFor(() => store.hasActiveDevice() === true).then(() => {
-          this.threshold = store.getActiveDevice().mic_status.noise_gate.threshold
-          this.attack = store.getActiveDevice().mic_status.noise_gate.attack
-          this.release = store.getActiveDevice().mic_status.noise_gate.release
-          this.attenuation = store.getActiveDevice().mic_status.noise_gate.attenuation
-          this.updateAmount(this.threshold)
-        }
-    );
-  },
 
   methods: {
+    getAmount() {
+      return Math.round((this.getThreshold() + 59) / 59 * 100)
+    },
+
+    getThreshold() {
+      if (!store.hasActiveDevice()) {
+        return 0;
+      }
+      return store.getActiveDevice().mic_status.noise_gate.threshold;
+    },
+
+    getAttack() {
+      if (!store.hasActiveDevice()) {
+        return 0;
+      }
+      return store.getActiveDevice().mic_status.noise_gate.attack;
+    },
+
+    getRelease() {
+      if (!store.hasActiveDevice()) {
+        return 0;
+      }
+      return store.getActiveDevice().mic_status.noise_gate.release;
+    },
+
+    getAttenuation() {
+      if (!store.hasActiveDevice()) {
+        return 0;
+      }
+
+      return store.getActiveDevice().mic_status.noise_gate.attenuation;
+    },
+
     isAdvanced() {
       if (!store.hasActiveDevice()) {
         return false;
@@ -58,54 +78,61 @@ export default {
       websocket.send_command(store.getActiveSerial(), {"SetElementDisplayMode": ["NoiseGate", mode]})
     },
 
-    waitFor(conditionFunction) {
-      const poll = resolve => {
-        if (conditionFunction()) resolve();
-        else setTimeout(() => poll(resolve), 400);
-      }
-      return new Promise(poll);
-    },
-
     setValue: function (id, value) {
       switch (id) {
-        case 0: {
-          this.updateThreshold(value)
-          this.commitValue("SetGateThreshold", this.threshold);
-        } break
-        case 1: {
+        case 0:
+          this.commitValue("SetGateThreshold", this.calculateThreshold(value));
+          break
+        case 1:
           this.commitValue("SetGateThreshold", value);
-          this.updateAmount(value)
-        } break
-        case 2: this.commitValue("SetGateAttenuation", value); break
-        case 3: this.commitValue("SetGateAttack", value); break
-        case 4: this.commitValue("SetGateRelease", value); break
+          break
+        case 2:
+          this.commitValue("SetGateAttenuation", value);
+          break
+        case 3:
+          this.commitValue("SetGateAttack", value);
+          break
+        case 4:
+          this.commitValue("SetGateRelease", value);
+          break
       }
     },
 
-    commitValue: function (name, value){
+    commitValue: function (name, value) {
       let serial = store.getActiveSerial();
 
       let command = {
         [name]: value
       }
       websocket.send_command(serial, command);
+
+      switch (name) {
+        case "SetGateThreshold":
+          store.getActiveDevice().mic_status.noise_gate.threshold = value;
+          break;
+        case "SetGateAttenuation":
+          store.getActiveDevice().mic_status.noise_gate.attenuation = value;
+          break;
+        case "SetGateAttack":
+          store.getActiveDevice().mic_status.noise_gate.attack = value;
+          break;
+        case "SetGateRelease":
+          store.getActiveDevice().mic_status.noise_gate.release = value;
+          break;
+      }
     },
 
-    getGateValueMap: function (){
+    getGateValueMap: function () {
       return ["10", "20", "30", "40", "50", "60", "70", "80", "90", "100", "110", "120", "130", "140", "150", "160",
         "170", "180", "190", "200", "250", "300", "350", "400", "450", "500", "550", "600", "650", "700", "750", "800",
         "850", "900", "950", "1000", "1200", "1300", "1400", "1500", "1600", "1700", "1800", "1900", "2000"];
     },
 
-    updateAmount: function (value){
-      this.amount = Math.round((value + 59) / 59 * 100);
+    calculateThreshold: function (value) {
+      return Math.round(value / 100 * 59) - 59;
     },
 
-    updateThreshold: function (value) {
-      this.threshold = Math.round(value / 100 * 59) - 59;
-    },
-
-    getStorePath: function(target) {
+    getStorePath: function (target) {
       return "/mixers/" + store.getActiveSerial() + "/mic_status/noise_gate/" + target;
     }
   }
