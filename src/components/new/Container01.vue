e1076d8b324bec0a63e5b0adaff9387b53511b7c
DonutCrab <dev@donutcrab.com>
2023-01-04 21:11:34 +0100

    Containers

---
+++ src/components/new/Container01.vue
@@ -0,0 +1,49 @@
+<script>
+export default {
+    name: "Container-1",
+    props: {
+        title: String,
+        leftMargin: {type: Boolean, required: false, default: true}
+    }
+}
+</script>
+
+<template>
+    <div class="container" :class="{'left-margin': leftMargin}">
+        <p class="title">
+            {{ title }}
+        </p>
+        <slot></slot>
+    </div>
+</template>
+
+<style scoped>
+* {
+    margin: 0;
+    padding: 0;
+}
+
+.container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    height: 220px;
+    width: 200px;
+
+    background-color: #353937;
+}
+
+.left-margin {
+    margin-left: 24px;
+}
+
+.title {
+    width: 100%;
+    padding: 10px 0;
+
+    color: #ffffff;
+    background-color: #3b413f;
+    text-transform: uppercase;
+}
+</style>
---
+++ src/components/new/Container02.vue
@@ -0,0 +1,52 @@
+<script>
+export default {
+    name: "Container-2",
+    props: {
+        title: String
+    }
+}
+</script>
+
+<template>
+    <div class="container">
+        <p class="title">
+            {{ title }}
+        </p>
+        <div class="content">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+* {
+    margin: 0;
+    padding: 0;
+}
+
+.container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    width: fit-content;
+    padding: 20px;
+
+    background-color: #2D3230;
+}
+
+.title {
+    padding: 0 0 10px 0;
+
+    color: #ffffff;
+    text-transform: uppercase;
+}
+
+.content {
+    display: flex;
+    flex-direction: row;
+
+    height: fit-content;
+    width: fit-content;
+}
+</style>
