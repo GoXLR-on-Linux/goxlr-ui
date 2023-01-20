99fbefd804b1839ab989f2504c2fc4974c74f558
Craig McLure <craig@mclure.net>
2023-01-20 12:47:43 +0000

    Removed xmlns

--- src/components/profiles/ProfileManager.vue
+++ src/components/profiles/ProfileManager.vue
@@ -1,4 +1,4 @@
-<template xmlns:slot="http://www.w3.org/1999/html">
+<template>
   <ProfileButtonList ref="buttonList">
     <ProfileButton v-for="(name, index) in profileList" :key="index" :button-id="name"
                    :label="name" :is-selected="isSelectedProfile(name)" :is-active="isActiveProfile(name)"
