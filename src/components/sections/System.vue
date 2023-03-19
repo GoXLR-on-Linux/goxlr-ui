ad6efe60f3c184095678edffe68c344818ef490f
Craig McLure <craig@mclure.net>
2023-03-19 16:10:27 +0000

    Complete Migration of System tab to nicer stuff.

--- src/components/sections/System.vue
+++ src/components/sections/System.vue
@@ -6,47 +6,31 @@
         <div><SettingsButton /></div>
         <div><AboutButton /></div>
         <div><LicenseButton /></div>
-
+        <div><HelpButton /></div>
       </div>
-
-      <MicSetupButton/>
-      <div style="margin-right: 30px"></div>
-      <BigButton id="settings" title="Settings" @button-clicked="showSettingsModal = true">
-        <font-awesome-icon icon="fa-solid fa-gear"/>
-      </BigButton>
-      <div style="margin-right: 30px"></div>
-      <AboutButton/>
-      <div style="margin-right: 30px"></div>
-      <LicenseButton/>
-      <div style="margin-right: 30px"></div>
-      <a :href="'https://discord.gg/Wbp3UxkX2j'">
-        <BigButton id="help" :title="'Help'">
-          <font-awesome-icon icon="fa-solid fa-circle-question"/>
-        </BigButton>
-      </a>
     </GroupContainer>
   </CenteredContainer>
 </template>
 
 <script>
-import BigButton from "@/components/big_buttons/BigButton";
-import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
 import MicSetupButton from "@/components/sections/system/modals/MicSetupButton.vue";
 import CenteredContainer from "@/components/containers/CenteredContainer.vue";
 import GroupContainer from "@/components/containers/GroupContainer.vue";
 import LicenseButton from "@/components/sections/system/modals/LicenseButton.vue";
 import AboutButton from "@/components/sections/system/modals/AboutButton.vue";
 import SettingsButton from "@/components/sections/system/modals/SettingsButton.vue";
+import HelpButton from "@/components/sections/system/HelpButton.vue";
 
 export default {
   name: "SystemComponent",
   components: {
+    HelpButton,
     SettingsButton,
     AboutButton,
     LicenseButton,
     GroupContainer,
     CenteredContainer,
-    MicSetupButton, BigButton, FontAwesomeIcon
+    MicSetupButton
   },
 }
 </script>
---
+++ src/components/sections/system/HelpButton.vue
@@ -0,0 +1,20 @@
+<template>
+  <a :href="'https://discord.gg/Wbp3UxkX2j'">
+    <BigButton id="help" :title="'Help'">
+      <font-awesome-icon icon="fa-solid fa-circle-question"/>
+    </BigButton>
+  </a>
+</template>
+
+<script>
+import BigButton from "@/components/big_buttons/BigButton.vue";
+
+export default {
+  name: "HelpButton",
+  components: {BigButton}
+}
+</script>
+
+<style scoped>
+
+</style>
