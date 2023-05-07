bad5000ebe33662441ea92d86cb4ef1d978e1058
Abdulaziz Alshmasi <a2h.alshmasi@gmail.com>
2023-05-07 21:59:28 +0300

    Added Accessibility Support for Upper Ribbon and Lower Ribbon tabs with keyboard navigation

--- src/components/GoXLR.vue
+++ src/components/GoXLR.vue
@@ -1,122 +1,118 @@
 <template>
+    <div id="main">
+        <DeviceSelector v-if="!isDeviceSet()" />
 
-  <div id="main">
-    <DeviceSelector v-if="!isDeviceSet()"/>
+        <template v-if="isDeviceSet()">
+            <div style="display: flex; flex-direction: row; column-gap: 30px">
+                <div>
+                    <FileTabs />
+                </div>
+            </div>
 
-    <template v-if="isDeviceSet()">
-      <div style="display: flex; flex-direction: row; column-gap: 30px">
-        <div>
-          <FileTabs/>
-        </div>
+            <div style="height: 25px; background-color: #3b413f" />
 
-      </div>
-
-      <div style="height: 25px; background-color: #3b413f"/>
-
-      <Tabs>
-        <Tab name="Mic">
-          <Mic/>
-        </Tab>
-        <Tab name="Mixer" selected>
-          <ContentContainer>
-            <Mixer/>
-          </ContentContainer>
-        </Tab>
-        <Tab name="Configuration">
-          <ContentContainer>
-            <CenteredContainer>
-              <Faders/>
-              <Cough/>
-            </CenteredContainer>
-          </ContentContainer>
-        </Tab>
-        <Tab v-if="!isDeviceMini()" name="Effects">
-          <EffectsTab/>
-        </Tab>
-        <Tab v-if="!isDeviceMini()" name="Sampler">
-          <ContentContainer>
-            <SamplerTab/>
-          </ContentContainer>
-        </Tab>
-        <Tab name="Lighting">
-          <LightingTab/>
-        </Tab>
-        <Tab name="Routing">
-          <ContentContainer>
-            <Routing/>
-          </ContentContainer>
-        </Tab>
-        <Tab name="System">
-          <ContentContainer>
-            <SystemComponent/>
-          </ContentContainer>
-        </Tab>
-      </Tabs>
-    </template>
-  </div>
-
+            <Tabs label="Lower Ribbon">
+                <Tab name="Mic">
+                    <Mic />
+                </Tab>
+                <Tab name="Mixer" selected>
+                    <ContentContainer>
+                        <Mixer />
+                    </ContentContainer>
+                </Tab>
+                <Tab name="Configuration">
+                    <ContentContainer>
+                        <CenteredContainer>
+                            <Faders />
+                            <Cough />
+                        </CenteredContainer>
+                    </ContentContainer>
+                </Tab>
+                <Tab v-if="!isDeviceMini()" name="Effects">
+                    <EffectsTab />
+                </Tab>
+                <Tab v-if="!isDeviceMini()" name="Sampler">
+                    <ContentContainer>
+                        <SamplerTab />
+                    </ContentContainer>
+                </Tab>
+                <Tab name="Lighting">
+                    <LightingTab />
+                </Tab>
+                <Tab name="Routing">
+                    <ContentContainer>
+                        <Routing />
+                    </ContentContainer>
+                </Tab>
+                <Tab name="System">
+                    <ContentContainer>
+                        <SystemComponent />
+                    </ContentContainer>
+                </Tab>
+            </Tabs>
+        </template>
+    </div>
 </template>
 
 <script>
-
-import Faders from "./sections/Faders"
+import Faders from "./sections/Faders";
 import Mixer from "./sections/Mixer";
 import Tabs from "@/components/tabs/Tabs";
 import Tab from "@/components/tabs/Tab";
 import Routing from "@/components/sections/Routing";
 import Mic from "@/components/sections/Mic";
 import DeviceSelector from "@/components/sections/DeviceSelector";
-import {store} from "@/store";
+import { store } from "@/store";
 import Cough from "@/components/sections/Cough";
-import {websocket} from "@/util/sockets";
+import { websocket } from "@/util/sockets";
 import SystemComponent from "@/components/sections/System";
 import FileTabs from "@/components/sections/files/FileTabs";
 import EffectsTab from "@/components/sections/EffectsTab";
-import {isDeviceMini} from "@/util/util";
+import { isDeviceMini } from "@/util/util";
 import LightingTab from "@/components/sections/lighting/LightingTab";
 import SamplerTab from "@/components/sections/SamplerTab";
 import ContentContainer from "@/components/containers/ContentContainer.vue";
 import CenteredContainer from "@/components/containers/CenteredContainer.vue";
 
 export default {
-  name: 'GoXLR',
-  components: {
-    CenteredContainer,
-    ContentContainer,
-    SamplerTab,
-    LightingTab,
-    EffectsTab,
-    FileTabs,
-    SystemComponent,
-    Cough,
-    DeviceSelector,
-    Routing,
-    Tab,
-    Tabs,
-    Mixer,
-    Faders,
-    Mic
-  },
-
-  methods: {
-    isDeviceMini,
-
-    isDeviceSet() {
-      return (store.hasActiveDevice() && store.isConnected());
+    name: "GoXLR",
+    components: {
+        CenteredContainer,
+        ContentContainer,
+        SamplerTab,
+        LightingTab,
+        EffectsTab,
+        FileTabs,
+        SystemComponent,
+        Cough,
+        DeviceSelector,
+        Routing,
+        Tab,
+        Tabs,
+        Mixer,
+        Faders,
+        Mic,
     },
-  },
 
-  created() {
-    websocket.get_status().then((data) => {
-      store.replaceData(data);
-    });
-  },
-}
+    methods: {
+        isDeviceMini,
+
+        isDeviceSet() {
+            return store.hasActiveDevice() && store.isConnected();
+        },
+    },
+
+    created() {
+        websocket.get_status().then((data) => {
+            store.replaceData(data);
+        });
+    },
+};
 </script>
 
 <style scoped>
 #main {
-  width: 100%;
-  font-size: 10pt;
+    width: 100%;
+    font-size: 10pt;
 }
 </style>
--- src/components/tabs/Tabs.vue
+++ src/components/tabs/Tabs.vue
@@ -1,77 +1,131 @@
 <template>
-  <div style="margin-left: 8px; margin-right: 8px">
-    <div class="tab">
-      <button v-for="tab in tabs" :key="tab.name" :class="{ 'active': tab.isActive }" v-show="!tab.hidden" @click="selectTab(tab)">{{tab.name}}</button>
+    <div style="margin-left: 8px; margin-right: 8px">
+        <div class="tab" role="TabList" :aria-label="this.label">
+            <button
+                v-for="tab in tabs"
+                :key="tab.name"
+                :class="{ active: tab.isActive }"
+                v-show="!tab.hidden"
+                @click="selectTab(tab)"
+                role="tab"
+                :aria-selected="tab.isActive"
+                :tabindex="tab.isActive ? 0 : -1"
+                @keydown="onTabKeydown"
+                :ref="tab.name"
+            >
+                {{ tab.name }}
+            </button>
+        </div>
+        <div
+            class="tabs-details"
+            role="tabpanel"
+            :aria-label="getActiveTab().name"
+        >
+            <slot></slot>
+        </div>
     </div>
-    <div class="tabs-details">
-      <slot></slot>
-    </div>
-  </div>
 </template>
 
 <script>
 export default {
-  name: "TabList",
+    name: "TabList",
 
-  data() {
-    return {tabs: []};
-  },
+    data() {
+        return { tabs: [] };
+    },
+    props: {
+        label: {
+            type: String,
+            required: false,
+            default: "Tab list",
+        },
+    },
 
-  created() {
+    created() {},
 
-  },
+    methods: {
+        selectTab(selectedTab) {
+            this.tabs.forEach((tab) => {
+                tab.isActive = tab.name === selectedTab.name;
+            });
+        },
+        getActiveTab() {
+            //return the active tab
+            const activeTab = this.tabs.find((tab) => tab.isActive);
+            if (activeTab) {
+                return activeTab;
+            } else {
+                return "";
+            }
+        },
+        //keyboard navigation
+        onTabKeydown(event) {
+            const tabs = this.tabs;
+            const activeTab = this.getActiveTab();
+            const activeTabIndex = tabs.indexOf(activeTab);
+            let nextTab;
 
-  methods: {
-    selectTab(selectedTab) {
-      this.tabs.forEach(tab => {
-        tab.isActive = (tab.name === selectedTab.name)
-      });
-    }
-  }
-}
+            if (event.key === "ArrowRight" || event.key === "ArrowDown") {
+                nextTab = tabs[(activeTabIndex + 1) % tabs.length];
+                //explanation: if activeTabIndex is 0, then 0+1 % 3 = 1, so nextTab is tabs[1]
+            } else if (event.key === "ArrowLeft" || event.key === "ArrowUp") {
+                nextTab =
+                    tabs[(activeTabIndex - 1 + tabs.length) % tabs.length];
+                //explanation: if activeTabIndex is 0, then 0-1+3 % 3 = 2, so nextTab is tabs[2]
+            } else if (event.key === "Home") {
+                nextTab = tabs[0];
+            } else if (event.key === "End") {
+                nextTab = tabs[tabs.length - 1];
+            }
+
+            if (nextTab) {
+                this.selectTab(nextTab);
+                //nextTab.$el is the button element
+                //we need a ref on the button element to focus it
+                this.$refs[nextTab.name][0].focus();
+            }
+        },
+    },
+};
 </script>
 
 <style>
 .tab {
-  border-bottom: 1px solid #59b1b6;
-  text-align: left;
+    border-bottom: 1px solid #59b1b6;
+    text-align: left;
 }
 
 .tab button {
-  background-color: inherit;
-  border: none;
-  outline: none;
-  cursor: pointer;
-  padding: 10px 20px;
-  margin-bottom: -1px;
-  width: 150px;
+    background-color: inherit;
+    border: none;
+    outline: none;
+    cursor: pointer;
+    padding: 10px 20px;
+    margin-bottom: -1px;
+    width: 150px;
 
-  /*font-family: LeagueMonoVariable, sans-serif;*/
-  border-radius: 5px 5px 0 0;
-  color: #fff;
+    /*font-family: LeagueMonoVariable, sans-serif;*/
+    border-radius: 5px 5px 0 0;
+    color: #fff;
 }
 
 .tab button:hover:not(.active) {
-  background-color: #2d3230;
+    background-color: #2d3230;
 }
 
 .tab button.active {
-  border: 1px solid #59b1b6;
-  border-bottom: 1px solid #252927;
+    border: 1px solid #59b1b6;
+    border-bottom: 1px solid #252927;
 
-
-  text-shadow:
-      0 0 3px #59b1b6,
-      0 0 5px #59b1b6;
+    text-shadow: 0 0 3px #59b1b6, 0 0 5px #59b1b6;
 }
 
 .tabs-details {
-  border: 1px solid #59b1b6;
-  border-top: 0;
-  padding: 0;
-  margin: 0;
-  overflow: auto;
-  vertical-align: middle;
+    border: 1px solid #59b1b6;
+    border-top: 0;
+    padding: 0;
+    margin: 0;
+    overflow: auto;
+    vertical-align: middle;
 }
-
 </style>
