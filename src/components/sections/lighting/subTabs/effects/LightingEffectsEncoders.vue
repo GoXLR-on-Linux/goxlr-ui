42bc974ab7fcc13d0ce1688c472017df2021fc28
Craig McLure <craig@mclure.net>
2023-05-11 22:00:29 +0100

    Added 'Apply to All' for Encoders

--- src/components/sections/lighting/subTabs/effects/LightingEffectsEncoders.vue
+++ src/components/sections/lighting/subTabs/effects/LightingEffectsEncoders.vue
@@ -3,116 +3,147 @@ import GroupContainer from "@/components/containers/GroupContainer.vue";
 import RadioSelection from "@/components/lists/RadioSelection.vue";
 import ColourPicker from "@/components/sections/lighting/elements/ColourPicker.vue";
 
-import { store } from "@/store";
-import { websocket } from "@/util/sockets";
-import { LightingInactiveOptions } from "@/util/mixerMapping";
+import {store} from "@/store";
+import {websocket} from "@/util/sockets";
+import {LightingInactiveOptions} from "@/util/mixerMapping";
 
 export default {
-    name: "LightingEffectsEncoders",
-    components: {
-        GroupContainer,
-        RadioSelection,
-        ColourPicker
-    },
+  name: "LightingEffectsEncoders",
+  components: {
+    GroupContainer,
+    RadioSelection,
+    ColourPicker
+  },
 
-    data() {
-        return {
-            encoderOptions: [
-                {
-                    id: 'Reverb',
-                    label: 'Reverb'
-                },
-                {
-                    id: 'Echo',
-                    label: 'Echo'
-                },
-                {
-                    id: 'Pitch',
-                    label: 'Pitch'
-                },
-                {
-                    id: 'Gender',
-                    label: 'Gender'
-                },
-            ],
-            selectedEncoder: "Reverb",
-            inactiveOptions: LightingInactiveOptions
-        }
-    },
-
-    methods: {
-        leftColour() {
-            return "#" + store.getActiveDevice().lighting.encoders[this.selectedEncoder]["colour_two"]
+  data() {
+    return {
+      encoderOptions: [
+        {
+          id: 'Reverb',
+          label: 'Reverb'
         },
-
-        rightColour() {
-            return "#" + store.getActiveDevice().lighting.encoders[this.selectedEncoder]["colour_one"]
+        {
+          id: 'Echo',
+          label: 'Echo'
         },
-
-        knobColour() {
-            return "#" + store.getActiveDevice().lighting.encoders[this.selectedEncoder]["colour_three"]
+        {
+          id: 'Pitch',
+          label: 'Pitch'
         },
-
-        onButtonSelectionChange(id) {
-            this.selectedEncoder = id
+        {
+          id: 'Gender',
+          label: 'Gender'
         },
-
-        onLeftColourChange(value) {
-            const encoder = store.getActiveDevice().lighting.encoders[this.selectedEncoder];
-
-            const leftColour = value.substr(1, 6);
-            const rightColour = encoder.colour_one;
-            const knobColour = encoder.colour_three;
-
-            websocket.send_command(store.getActiveSerial(), {"SetEncoderColour": [this.selectedEncoder, rightColour, leftColour, knobColour]})
-        },
-
-        onRightColourChange(value) {
-            const encoder = store.getActiveDevice().lighting.encoders[this.selectedEncoder];
-
-            const leftColour = encoder.colour_two;
-            const rightColour = value.substr(1, 6);
-            const knobColour = encoder.colour_three;
-
-            websocket.send_command(store.getActiveSerial(), {"SetEncoderColour": [this.selectedEncoder, rightColour, leftColour, knobColour]})
-        },
-
-        onKnobColourChange(value) {
-            const encoder = store.getActiveDevice().lighting.encoders[this.selectedEncoder];
-
-            const leftColour = encoder.colour_two;
-            const rightColour = encoder.colour_one;
-            const knobColour = value.substr(1, 6);
-
-            websocket.send_command(store.getActiveSerial(), {"SetEncoderColour": [this.selectedEncoder, rightColour, leftColour, knobColour]})
-        }
+      ],
+      selectedEncoder: "Reverb",
+      inactiveOptions: LightingInactiveOptions
     }
+  },
+
+  methods: {
+    leftColour() {
+      return "#" + store.getActiveDevice().lighting.encoders[this.selectedEncoder]["colour_two"]
+    },
+
+    rightColour() {
+      return "#" + store.getActiveDevice().lighting.encoders[this.selectedEncoder]["colour_one"]
+    },
+
+    knobColour() {
+      return "#" + store.getActiveDevice().lighting.encoders[this.selectedEncoder]["colour_three"]
+    },
+
+    onButtonSelectionChange(id) {
+      this.selectedEncoder = id
+    },
+
+    onLeftColourChange(value) {
+      const encoder = store.getActiveDevice().lighting.encoders[this.selectedEncoder];
+
+      const leftColour = value.substr(1, 6);
+      const rightColour = encoder.colour_one;
+      const knobColour = encoder.colour_three;
+
+      websocket.send_command(store.getActiveSerial(), {"SetEncoderColour": [this.selectedEncoder, rightColour, leftColour, knobColour]})
+    },
+
+    onRightColourChange(value) {
+      const encoder = store.getActiveDevice().lighting.encoders[this.selectedEncoder];
+
+      const leftColour = encoder.colour_two;
+      const rightColour = value.substr(1, 6);
+      const knobColour = encoder.colour_three;
+
+      websocket.send_command(store.getActiveSerial(), {"SetEncoderColour": [this.selectedEncoder, rightColour, leftColour, knobColour]})
+    },
+
+    onKnobColourChange(value) {
+      const encoder = store.getActiveDevice().lighting.encoders[this.selectedEncoder];
+
+      const leftColour = encoder.colour_two;
+      const rightColour = encoder.colour_one;
+      const knobColour = value.substr(1, 6);
+
+      websocket.send_command(store.getActiveSerial(), {"SetEncoderColour": [this.selectedEncoder, rightColour, leftColour, knobColour]})
+    },
+
+    applyToAll() {
+      const encoder = store.getActiveDevice().lighting.encoders[this.selectedEncoder];
+      const rightColour = encoder.colour_one;
+      const leftColour = encoder.colour_two;
+      const knobColour = encoder.colour_three;
+
+      for (let encoder in this.encoderOptions) {
+        let id = this.encoderOptions[encoder].id;
+        websocket.send_command(store.getActiveSerial(), {"SetEncoderColour": [id, rightColour, leftColour, knobColour]});
+      }
+    }
+  }
 }
 </script>
 
 <template>
-    <GroupContainer title="Encoders">
-      <RadioSelection
+  <GroupContainer title="Encoders">
+    <template #right>
+      <button class="applyToAll" @click="applyToAll()">Apply to All</button>
+    </template>
+    <RadioSelection
         title="Encoder"
         group="lighting_effects_encoders"
         :options="this.encoderOptions"
         :selected="this.selectedEncoder"
         @selection-changed="onButtonSelectionChange"
-      />
-      <ColourPicker
+    />
+    <ColourPicker
         title="Left Colour"
         :color-value="leftColour()"
         @colour-changed="onLeftColourChange"
-      />
-      <ColourPicker
+    />
+    <ColourPicker
         title="Right Colour"
         :color-value="rightColour()"
         @colour-changed="onRightColourChange"
-      />
-      <ColourPicker
+    />
+    <ColourPicker
         title="Knob Colour"
         :color-value="knobColour()"
         @colour-changed="onKnobColourChange"
-      />
-    </GroupContainer>
+    />
+  </GroupContainer>
 </template>
+
+<style scoped>
+button {
+  border: 0;
+  margin: 0;
+  padding: 5px 24px;
+  color: #fff;
+  font-family: LeagueMonoCondensed, sans-serif;
+  font-size: 12px;
+  background-color: #3b413f;
+}
+
+button:hover {
+  background-color: #535c59;
+}
+</style>
