834dff87bb39208a0c5c5da93d5c767b770bbb84
Craig McLure <craig@mclure.net>
2023-05-11 23:08:29 +0100

    Added 'Apply to All' for Sampler

--- src/components/sections/lighting/subTabs/LightingSampler.vue
+++ src/components/sections/lighting/subTabs/LightingSampler.vue
@@ -4,9 +4,9 @@ import ContentContainer from "@/components/containers/ContentContainer";
 import RadioSelection from "@/components/lists/RadioSelection.vue";
 import ColourPicker from "@/components/sections/lighting/elements/ColourPicker";
 
-import { store } from "@/store";
-import { websocket } from "@/util/sockets";
-import { LightingInactiveOptions } from "@/util/mixerMapping";
+import {store} from "@/store";
+import {websocket} from "@/util/sockets";
+import {LightingInactiveOptions} from "@/util/mixerMapping";
 import CenteredContainer from "@/components/containers/CenteredContainer.vue";
 
 export default {
@@ -23,38 +23,38 @@ export default {
     return {
       buttonOptions: [
         {
-          id: 'A',
+          id: 'SamplerSelectA',
           label: 'A'
         },
         {
-          id: 'B',
+          id: 'SamplerSelectB',
           label: 'B'
         },
         {
-          id: 'C',
+          id: 'SamplerSelectC',
           label: 'C'
         }
       ],
-      selectedButtonOption: 'A',
+      selectedButtonOption: 'SamplerSelectA',
       inactiveOptions: LightingInactiveOptions
     }
   },
 
   methods: {
     activeColor() {
-      return "#" + store.getActiveDevice().lighting.sampler[this.getActiveBank()].colours["colour_one"];
-    },
-
-    emptyColor() {
-      return "#" + store.getActiveDevice().lighting.sampler[this.getActiveBank()].colours["colour_three"];
+      return "#" + store.getActiveDevice().lighting.sampler[this.selectedButtonOption].colours["colour_one"];
     },
 
     inactiveColor() {
-      return "#" + store.getActiveDevice().lighting.sampler[this.getActiveBank()].colours["colour_two"];
+      return "#" + store.getActiveDevice().lighting.sampler[this.selectedButtonOption].colours["colour_two"];
+    },
+
+    emptyColor() {
+      return "#" + store.getActiveDevice().lighting.sampler[this.selectedButtonOption].colours["colour_three"];
     },
 
     selectedInactiveOption() {
-      return store.getActiveDevice().lighting.sampler[this.getActiveBank()].off_style
+      return store.getActiveDevice().lighting.sampler[this.selectedButtonOption].off_style
     },
 
     onButtonSelectionChange(id) {
@@ -62,37 +62,48 @@ export default {
     },
 
     onInactiveSelectionChange(id) {
-      websocket.send_command(store.getActiveSerial(), {"SetSampleOffStyle": [this.getActiveBank(), id]});
+      websocket.send_command(store.getActiveSerial(), {"SetSampleOffStyle": [this.selectedButtonOption, id]});
     },
 
     onActiveColourChange(value) {
       let colour_one = value.substr(1, 6);
-      let colour_two = store.getActiveDevice().lighting.sampler[this.getActiveBank()].colours.colour_two;
-      let colour_three = store.getActiveDevice().lighting.sampler[this.getActiveBank()].colours.colour_three;
+      let colour_two = store.getActiveDevice().lighting.sampler[this.selectedButtonOption].colours.colour_two;
+      let colour_three = store.getActiveDevice().lighting.sampler[this.selectedButtonOption].colours.colour_three;
 
-      websocket.send_command(store.getActiveSerial(), {"SetSampleColour": [this.getActiveBank(), colour_one, colour_two, colour_three]})
+      websocket.send_command(store.getActiveSerial(), {"SetSampleColour": [this.selectedButtonOption, colour_one, colour_two, colour_three]})
     },
 
     onEmptyColourChange(value) {
       console.log(store.getActiveDevice().lighting.sampler);
 
-      let colour_one = store.getActiveDevice().lighting.sampler[this.getActiveBank()].colours.colour_one;
-      let colour_two = store.getActiveDevice().lighting.sampler[this.getActiveBank()].colours.colour_two;
+      let colour_one = store.getActiveDevice().lighting.sampler[this.selectedButtonOption].colours.colour_one;
+      let colour_two = store.getActiveDevice().lighting.sampler[this.selectedButtonOption].colours.colour_two;
       let colour_three = value.substr(1, 6);
 
-      websocket.send_command(store.getActiveSerial(), {"SetSampleColour": [this.getActiveBank(), colour_one, colour_two, colour_three]})
+      websocket.send_command(store.getActiveSerial(), {"SetSampleColour": [this.selectedButtonOption, colour_one, colour_two, colour_three]})
     },
 
     onInactiveColourChange(value) {
-      let colour_one = store.getActiveDevice().lighting.sampler[this.getActiveBank()].colours.colour_one;
+      let colour_one = store.getActiveDevice().lighting.sampler[this.selectedButtonOption].colours.colour_one;
       let colour_two = value.substr(1, 6);
-      let colour_three = store.getActiveDevice().lighting.sampler[this.getActiveBank()].colours.colour_three;
+      let colour_three = store.getActiveDevice().lighting.sampler[this.selectedButtonOption].colours.colour_three;
 
-      websocket.send_command(store.getActiveSerial(), {"SetSampleColour": [this.getActiveBank(), colour_one, colour_two, colour_three]})
+      websocket.send_command(store.getActiveSerial(), {"SetSampleColour": [this.selectedButtonOption, colour_one, colour_two, colour_three]})
     },
 
-    getActiveBank() {
-      return "SamplerSelect" + this.selectedButtonOption;
+    applyToAll() {
+      const bank = store.getActiveDevice().lighting.sampler[this.selectedButtonOption].colours;
+      const activeColour = bank.colour_one;
+      const inactiveColour = bank.colour_two;
+      const emptyColour = bank.colour_three;
+
+      const off_style = this.selectedInactiveOption();
+
+      for (let encoder in this.buttonOptions) {
+        let id = this.buttonOptions[encoder].id;
+        websocket.send_command(store.getActiveSerial(), {"SetSampleColour": [id, activeColour, inactiveColour, emptyColour]});
+        websocket.send_command(store.getActiveSerial(), {"SetSampleOffStyle": [id, off_style]});
+      }
     }
   }
 }
@@ -102,12 +113,35 @@ export default {
   <CenteredContainer>
     <ContentContainer>
       <GroupContainer title="Bank">
-        <RadioSelection title="Buttons" group="lighting_sampler_buttons" :options="this.buttonOptions" :selected="this.selectedButtonOption" @selection-changed="onButtonSelectionChange"/>
-        <ColourPicker title="Active / Loaded" :color-value="activeColor()" @colour-changed="onActiveColourChange" />
-        <ColourPicker title="Sample Empty" :color-value="emptyColor()" @colour-changed="onEmptyColourChange" />
-        <RadioSelection title="Inactive Bank" group="lighting_sampler_inactive_behaviour" :options="this.inactiveOptions" :selected="this.selectedInactiveOption()" @selection-changed="onInactiveSelectionChange" />
-        <ColourPicker title="Inactive Bank" :color-value="inactiveColor()" @colour-changed="onInactiveColourChange" />
+        <template #right>
+          <button class="applyToAll" @click="applyToAll()">Apply to All</button>
+        </template>
+        <RadioSelection title="Buttons" group="lighting_sampler_buttons" :options="this.buttonOptions"
+                        :selected="this.selectedButtonOption" @selection-changed="onButtonSelectionChange"/>
+        <ColourPicker title="Active / Loaded" :color-value="activeColor()" @colour-changed="onActiveColourChange"/>
+        <ColourPicker title="Sample Empty" :color-value="emptyColor()" @colour-changed="onEmptyColourChange"/>
+        <RadioSelection title="Inactive Bank" group="lighting_sampler_inactive_behaviour"
+                        :options="this.inactiveOptions" :selected="this.selectedInactiveOption()"
+                        @selection-changed="onInactiveSelectionChange"/>
+        <ColourPicker title="Inactive Bank" :color-value="inactiveColor()" @colour-changed="onInactiveColourChange"/>
       </GroupContainer>
     </ContentContainer>
   </CenteredContainer>
 </template>
+
+
+<style scoped>
+button {
+  border: 0;
+  margin: 0;
+  padding: 5px 24px;
+  color: #fff;
+  font-family: LeagueMonoCondensed, sans-serif;
+  font-size: 12px;
+  background-color: #3b413f;
+}
+
+button:hover {
+  background-color: #535c59;
+}
+</style>
