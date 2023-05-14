cc163a658e2d443ed0dc44b3540fdfad4705d22a
Abdulaziz Alshmasi <a2h.alshmasi@gmail.com>
2023-05-14 22:27:05 +0300

    Integrated A11yNotifications Component into the Main Page

--- src/components/GoXLR.vue
+++ src/components/GoXLR.vue
@@ -1,62 +1,63 @@
 <template>
-  <div id="main">
-    <DeviceSelector v-if="!isDeviceSet()"/>
+    <div id="main">
+        <DeviceSelector v-if="!isDeviceSet()" />
 
-    <template v-if="isDeviceSet()">
+        <template v-if="isDeviceSet()">
+            <h1 class="screenreader-only">Profiles and Files</h1>
+            <div style="display: flex; flex-direction: row; column-gap: 30px">
+                <div>
+                    <FileTabs />
+                </div>
+            </div>
 
-      <h1 class="screenreader-only">Profiles and Files</h1>
-      <div style="display: flex; flex-direction: row; column-gap: 30px">
-        <div>
-          <FileTabs/>
-        </div>
-      </div>
-
-      <div style="height: 25px; background-color: #3b413f"/>
-      <h1 class="sr-only">Device Settings</h1>
-      <Tabs label="Device Settings">
-        <Tab name="Mic">
-          <Mic/>
-        </Tab>
-        <Tab name="Mixer" selected>
-          <ContentContainer>
-            <Mixer/>
-          </ContentContainer>
-        </Tab>
-        <Tab name="Configuration">
-          <ContentContainer>
-            <CenteredContainer>
-              <Faders/>
-              <Cough/>
-            </CenteredContainer>
-          </ContentContainer>
-        </Tab>
-        <Tab v-if="!isDeviceMini()" name="Effects">
-          <EffectsTab/>
-        </Tab>
-        <Tab v-if="!isDeviceMini()" name="Sampler">
-          <ContentContainer>
-            <SamplerTab/>
-          </ContentContainer>
-        </Tab>
-        <Tab name="Lighting">
-          <LightingTab/>
-        </Tab>
-        <Tab name="Routing">
-          <ContentContainer>
-            <Routing/>
-          </ContentContainer>
-        </Tab>
-        <Tab name="System">
-          <ContentContainer>
-            <SystemComponent/>
-          </ContentContainer>
-        </Tab>
-      </Tabs>
-    </template>
-  </div>
+            <div style="height: 25px; background-color: #3b413f" />
+            <h1 class="sr-only">Device Settings</h1>
+            <Tabs label="Device Settings">
+                <Tab name="Mic">
+                    <Mic />
+                </Tab>
+                <Tab name="Mixer" selected>
+                    <ContentContainer>
+                        <Mixer />
+                    </ContentContainer>
+                </Tab>
+                <Tab name="Configuration">
+                    <ContentContainer>
+                        <CenteredContainer>
+                            <Faders />
+                            <Cough />
+                        </CenteredContainer>
+                    </ContentContainer>
+                </Tab>
+                <Tab v-if="!isDeviceMini()" name="Effects">
+                    <EffectsTab />
+                </Tab>
+                <Tab v-if="!isDeviceMini()" name="Sampler">
+                    <ContentContainer>
+                        <SamplerTab />
+                    </ContentContainer>
+                </Tab>
+                <Tab name="Lighting">
+                    <LightingTab />
+                </Tab>
+                <Tab name="Routing">
+                    <ContentContainer>
+                        <Routing />
+                    </ContentContainer>
+                </Tab>
+                <Tab name="System">
+                    <ContentContainer>
+                        <SystemComponent />
+                    </ContentContainer>
+                </Tab>
+            </Tabs>
+            <A11yNotifications />
+        </template>
+    </div>
 </template>
 
 <script>
+import A11yNotifications from "./A11yNotifications";
 import Faders from "./sections/Faders";
 import Mixer from "./sections/Mixer";
 import Tabs from "@/components/tabs/Tabs";
@@ -64,64 +65,65 @@ import Tab from "@/components/tabs/Tab";
 import Routing from "@/components/sections/Routing";
 import Mic from "@/components/sections/Mic";
 import DeviceSelector from "@/components/sections/DeviceSelector";
-import {store} from "@/store";
+import { store } from "@/store";
 import Cough from "@/components/sections/Cough";
-import {websocket} from "@/util/sockets";
+import { websocket } from "@/util/sockets";
 import SystemComponent from "@/components/sections/System";
 import FileTabs from "@/components/sections/files/FileTabs";
 import EffectsTab from "@/components/sections/EffectsTab";
-import {isDeviceMini} from "@/util/util";
+import { isDeviceMini } from "@/util/util";
 import LightingTab from "@/components/sections/lighting/LightingTab";
 import SamplerTab from "@/components/sections/SamplerTab";
 import ContentContainer from "@/components/containers/ContentContainer.vue";
 import CenteredContainer from "@/components/containers/CenteredContainer.vue";
 
 export default {
-  name: "GoXLR",
-  components: {
-    CenteredContainer,
-    ContentContainer,
-    SamplerTab,
-    LightingTab,
-    EffectsTab,
-    FileTabs,
-    SystemComponent,
-    Cough,
-    DeviceSelector,
-    Routing,
-    Tab,
-    Tabs,
-    Mixer,
-    Faders,
-    Mic,
-  },
-
-  methods: {
-    isDeviceMini,
-
-    isDeviceSet() {
-      return store.hasActiveDevice() && store.isConnected();
+    name: "GoXLR",
+    components: {
+        A11yNotifications,
+        CenteredContainer,
+        ContentContainer,
+        SamplerTab,
+        LightingTab,
+        EffectsTab,
+        FileTabs,
+        SystemComponent,
+        Cough,
+        DeviceSelector,
+        Routing,
+        Tab,
+        Tabs,
+        Mixer,
+        Faders,
+        Mic,
     },
-  },
 
-  created() {
-    websocket.get_status().then((data) => {
-      store.replaceData(data);
-    });
-  },
+    methods: {
+        isDeviceMini,
+
+        isDeviceSet() {
+            return store.hasActiveDevice() && store.isConnected();
+        },
+    },
+
+    created() {
+        websocket.get_status().then((data) => {
+            store.replaceData(data);
+        });
+    },
 };
 </script>
 
 <style>
 .screenreader-only {
-  position: absolute;
-  left: -10000px;
-  top: auto;
-  width: 1px;
-  height: 1px;
-  overflow: hidden;
+    position: absolute;
+    left: -10000px;
+    top: auto;
+    width: 1px;
+    height: 1px;
+    overflow: hidden;
 
-  /**
+    /**
     position: absolute;
   width: 1px;
   height: 1px;
@@ -135,7 +137,7 @@ export default {
 }
 
 #main {
-  width: 100%;
-  font-size: 10pt;
+    width: 100%;
+    font-size: 10pt;
 }
 </style>
