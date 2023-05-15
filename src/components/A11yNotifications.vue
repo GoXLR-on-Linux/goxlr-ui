372abba5244758ed14a67247e44017fd8690082c
Abdulaziz Alshmasi <a2h.alshmasi@gmail.com>
2023-05-15 18:30:33 +0300

    Removed unnecessary styles.

--- src/components/A11yNotifications.vue
+++ src/components/A11yNotifications.vue
@@ -1,47 +1,25 @@
 //this component is to show the notifications
 <template>
-    <div class="screenreader-only">
-        <div aria-live="assertive">
-            {{ assertiveNotification }}
-        </div>
-        <div aria-live="polite">
-            {{ politeNotification }}
-        </div>
+  <div class="screenreader-only">
+    <div aria-live="assertive">
+      {{ assertiveNotification }}
     </div>
+    <div aria-live="polite">
+      {{ politeNotification }}
+    </div>
+  </div>
 </template>
 <script>
 import { store } from "@/store";
 export default {
-    name: "A11yNotifications",
-    computed: {
-        assertiveNotification() {
-            return store.getAccessibilityNotification("assertive");
-        },
-        politeNotification() {
-            return store.getAccessibilityNotification("polite");
-        },
+  name: "A11yNotifications",
+  computed: {
+    assertiveNotification() {
+      return store.getAccessibilityNotification("assertive");
     },
+    politeNotification() {
+      return store.getAccessibilityNotification("polite");
+    },
+  },
 };
 </script>
-<style scoped>
-.screenreader-only {
-    position: absolute;
-    left: -10000px;
-    top: auto;
-    width: 1px;
-    height: 1px;
-    overflow: hidden;
-
-    /**
-    position: absolute;
-  width: 1px;
-  height: 1px;
-  padding: 0;
-  margin: -1px;
-  overflow: hidden;
-  clip: rect(0, 0, 0, 0);
-  white-space: nowrap;
-  border-width: 0;
-   */
-}
-</style>
