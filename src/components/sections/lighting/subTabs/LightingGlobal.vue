1b974c9f1c8ec83c0956aebfc05165c548391ff4
Craig McLure <craig@mclure.net>
2023-07-02 03:57:19 +0100

    Initial Support for Mod1/2

--- src/components/sections/lighting/subTabs/LightingGlobal.vue
+++ src/components/sections/lighting/subTabs/LightingGlobal.vue
@@ -7,10 +7,12 @@ import ColourPicker from "@/components/sections/lighting/elements/ColourPicker.v
 import {store} from "@/store";
 import {websocket} from "@/util/sockets";
 import CenteredContainer from "@/components/containers/CenteredContainer.vue";
+import RangeSelector from "@/components/slider/components/Range.vue";
 
 export default {
   name: "LightingGlobal",
   components: {
+    RangeSelector,
     CenteredContainer,
     ContentContainer,
     GroupContainer,
@@ -57,7 +59,9 @@ export default {
         }
       ],
 
-      selected: 'Global'
+      selected: 'Global',
+      mod1Value: 0,
+      mod2Value: 0,
     }
   },
 
@@ -89,6 +93,42 @@ export default {
     onAnimationModeChange(id) {
       websocket.send_command(store.getActiveSerial(), { "SetAnimationMode": id });
     },
+
+    // Mod1 Settings..
+    getMod1StorePath() {
+      return "/mixers/S201200586CQK/lighting/animation/mod1"
+    },
+
+    getMod1Value() {
+      return store.getActiveDevice().lighting.animation.mod1;
+    },
+
+    setMod1Value(value) {
+      value = parseInt(value);
+
+      store.getActiveDevice().lighting.animation.mod1 = value;
+      websocket.send_command(store.getActiveSerial(), { "SetAnimationMod1": value });
+    },
+
+    getMod2StorePath() {
+      return "/mixers/S201200586CQK/lighting/animation/mod2"
+    },
+
+    getMod2Value() {
+      return store.getActiveDevice().lighting.animation.mod2;
+    },
+
+    setMod2Value(value) {
+      value = parseInt(value);
+
+      store.getActiveDevice().lighting.animation.mod2 = value;
+      websocket.send_command(store.getActiveSerial(), { "SetAnimationMod2": value });
+    }
+
+  },
+  mounted() {
+    this.mod1Value = store.getActiveDevice().lighting.animation.mod1;
+    this.mod2Value = store.getActiveDevice().lighting.animation.mod2;
   }
 }
 </script>
@@ -104,6 +144,27 @@ export default {
       <GroupContainer v-if="animationSupported()" title="Animations">
         <RadioSelection title="Animation Mode" group="lighting_animation" :options="this.animation_mode_options"
                         :selected="this.animationModeSelected()" @selection-changed="onAnimationModeChange"/>
+
+        <div style="text-align: center; color: #fff; padding-left: 8px">
+          <div style="margin-bottom: 10px">Gradient Mod 1</div>
+          <RangeSelector
+              :store-path=getMod1StorePath()
+              :current-field-value=mod1Value
+              @value-updated="setMod1Value"
+              :needs-rotation="false" :height=180
+              />
+          <div style="margin-top: 12px; margin-bottom: 20px; color: #82CFD0">{{getMod1Value()}}</div>
+
+          <div style="margin-bottom: 10px">Gradient Mod 2</div>
+          <RangeSelector
+              :store-path=getMod2StorePath()
+              :current-field-value=mod2Value
+              @value-updated="setMod2Value"
+              :needs-rotation="false" :height=180
+          />
+          <div style="margin-top: 12px; color: #82CFD0">{{getMod2Value()}}</div>
+
+        </div>
       </GroupContainer>
 
     </ContentContainer>
