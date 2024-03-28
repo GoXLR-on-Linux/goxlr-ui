97d2bd5f08349f34234f30f61673d8034869e81c
Craig McLure <craig@mclure.net>
2024-03-28 10:41:45 +0000

    Show correct channel name on Mix Assignment

--- src/components/sections/mixer/MixAssignment.vue
+++ src/components/sections/mixer/MixAssignment.vue
@@ -7,7 +7,7 @@
       <AssignmentEntry name="BroadcastMix" :display="$t('message.channels.StreamMix')"/>
       <AssignmentEntry name="LineOut" :display="$t('message.channels.LineOut')"/>
       <AssignmentEntry name="ChatMic" :display="$t('message.channels.ChatMic')"/>
-      <AssignmentEntry name="Sampler" :display="$t('message.channels.Sampler')"/>
+      <AssignmentEntry name="Sampler" :display="$t(getSamplerChannelString())"/>
     </div>
   </GroupContainer>
 </template>
@@ -16,9 +16,26 @@
 import AssignmentEntry from "@/components/sections/mixer/AssignmentEntry.vue";
 import {OutputDevice} from "@/util/mixerMapping";
 import GroupContainer from "@/components/containers/GroupContainer.vue";
+import {store} from "@/store";
+import {isDeviceMini, versionNewerOrEqualTo} from "@/util/util";
 
 export default {
   name: "MixAssignment",
+  methods: {
+    getSamplerChannelString() {
+      let sample = "message.channels.Sampler";
+      let vod = "message.channels.VOD";
+
+      if (store.hasActiveDevice()) {
+        if (isDeviceMini()) {
+          if (versionNewerOrEqualTo(store.getConfig().driver_interface.version, [5,30,0])) {
+            return vod;
+          }
+        }
+      }
+      return sample;
+    },
+  },
   computed: {
     OutputDevice() {
       return OutputDevice
