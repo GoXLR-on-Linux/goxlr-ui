827bde86e48a905ecdb06981630db20b20950014
Craig McLure <craig@mclure.net>
2024-08-01 04:15:41 +0100

    Added a firmware update check to the Version section

--- src/components/VersionCheck.vue
+++ src/components/VersionCheck.vue
@@ -2,14 +2,30 @@
   <div v-if="hasVersion()" class="version">
     GoXLR Utility v{{getVersion()}}
     <span v-if="outdated()"> - <a :href="release_path" target="_blank"> Update Available</a></span>
+    <span v-if="firmware_outdated()"> - <span class="click" @click="$refs.firmware_modal.openModal(undefined, undefined)">Firmware Update Available</span></span>
   </div>
+
+  <AccessibleModal width="630px" ref="firmware_modal" id="firmware_modal" :show_footer="true">
+    <template v-slot:title>Firmware Update Available</template>
+    <template v-slot:default>
+      A firmware update (Version {{getFirmwareVersion()}}) is available for your device.<br /><br />
+
+      At this point in time the GoXLR Utility is unable to perform a firmware update. Please plug your device
+      into a Windows based machine and run the official GoXLR Application to receive the latest firmware.<br /><br />
+
+      Once completed, re-launch the GoXLR Utility to continue using your GoXLR.
+    </template>
+  </AccessibleModal>
 </template>
 
 <script>
 import {store} from "@/store";
+import {isDeviceMini} from "@/util/util";
+import AccessibleModal from "@/components/design/modal/AccessibleModal.vue";
 
 export default {
   name: "VersionCheck",
+  components: {AccessibleModal},
 
   data: function () {
     return {
@@ -49,38 +65,91 @@ export default {
       return store.daemonVersion();
     },
 
+    getFirmwareVersion() {
+      if (store.getActiveDevice() === undefined) {
+        return false;
+      }
+
+      if (store.getConfig().latest_firmware !== undefined) {
+        let latest = isDeviceMini() ? store.getConfig().latest_firmware.Mini : store.getConfig().latest_firmware.Full;
+        return latest.join(".");
+      }
+      return true;
+    },
+
     outdated() {
       if (store.daemonVersion() === undefined) {
         return false;
       }
-      return this.isOutdated(store.daemonVersion());
+      return this.isOutdated(store.daemonVersion(), this.version);
+    },
+
+    firmware_outdated() {
+      if (store.getActiveDevice() === undefined) {
+        return false;
+      }
+
+      if (store.getConfig().latest_firmware !== undefined) {
+        let latest = isDeviceMini() ? store.getConfig().latest_firmware.Mini : store.getConfig().latest_firmware.Full;
+        let current = store.getActiveDevice().hardware.versions.firmware;
+
+        console.log(`${latest}`);
+
+        latest = latest.join(".");
+        current = current.join(".");
+
+        console.log(`${latest} - ${current}`);
+
+        return this.isOutdated(latest, current);
+      }
+
+      // Fail Safe if versions are missing..
+      return false;
     },
 
     getPath() {
       return "https://api.github.com/repos/GoXLR-on-Linux/goxlr-utility/releases";
     },
 
-    isOutdated(util_version) {
-      if (this.version === undefined) {
+    isOutdated(base_version, match_version) {
+      if (match_version === undefined) {
         // If we can't map the current version, assume they match to avoid bugging the user.
         return false;
       }
 
-      let util = util_version.split(".");
-      let version = this.version.split(".");
+      let base = base_version.split(".");
+      let match = match_version.split(".");
 
       // This isn't perfect, but it doesn't need to be. If the util is reporting a different version from github, there
       // has likely been an update, we'll deal with edge cases separately..
-
-      if (util[0] !== version[0]) {
+      if (base[0] !== match[0]) {
         return true;
       }
 
-      if (util[1] !== version[1]) {
+      if (base[1] !== match[1]) {
         return true;
       }
 
-      return util[2] !== version[2];
+      if (base[2] !== undefined) {
+        if (match[2] !== undefined) {
+          if (base[2] !== match[2]) {
+            return true;
+          }
+        } else {
+          return true;
+        }
+      }
+
+      if (base[3] !== undefined) {
+        if (match[3] !== undefined) {
+          if (base[3] !== match[3]) {
+            return true;
+          }
+        } else {
+          return true;
+        }
+      }
+      return false;
     }
   },
 
@@ -101,5 +170,11 @@ export default {
 
  .version a {
    color: #555555;
+ }
+
+ .version span.click {
+   color: #555555;
+   cursor: pointer;
+   text-decoration: underline;
  }
 </style>
